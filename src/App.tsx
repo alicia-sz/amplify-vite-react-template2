bf70f430409dc8e7199ae1fe69568d3db70492d9
Alicia Sanchez <alicia.d.sanchez@outlook.com>
2025-05-04 14:34:18 -0400

    last deployment failed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
-import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { Authenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { user, signOut } = useAuthenticator();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -18,10 +16,6 @@ function App() {
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
   }
 
   return (
